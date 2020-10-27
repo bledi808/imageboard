e16864c32cc0c90c6e1c4cd1c4262f483294523b
Bledi Hasa <bledi.hasa@gmail.com>
2020-10-27 11:45:33 +0100

    start of part 2

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+app.use(express.static("public"));
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 ///////////////////////////////////////middleware
-app.use(express.static("public"));
 
 ///////////////////////////////////////routes
 app.get("/images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
-            console.log("result", rows);
-            console.log("rows.length", rows.length);
+            // console.log("result", rows);
+            // console.log("rows.length", rows.length);
             res.json(rows);
         })
         .catch((err) => {
             console.log("error in /citiies with getImages()", err);
         });
     console.log();
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    //multer adds the file and the body to the request obj - req.body
+    //for Part 2; do a db query to insert all of this info into the db (to do this; we need the following file properties: filename and )
+    //once inserted; send all of this info to vue
+    console.log("input values: ", req.body);
+    console.log("file: ", req.file);
+    if (req.file) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
 });
 
 app.listen(8080, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -8,13 +8,48 @@ new Vue({
     el: "#container",
     data: {
         images: [],
+        //data properties that will store vlaues of input fields
+        title: "",
+        description: "",
+        username: "",
+        file: null,
     },
     mounted: function () {
         const me = this;
-        axios.get("/images").then(function (response) {
-            me.images = response.data; // works
-            console.log(me.images);
-        });
+        axios
+            .get("/images")
+            .then(function (response) {
+                me.images = response.data; // works
+                // console.log(me.images);
+            })
+            .catch(function (err) {
+                console.log("error in POST /images", err);
+            });
+    },
+    method: {
+        handleClick: function (e) {
+            e.preventDefault();
+            // console.log("this", this); // this referes to the object we are inside of (i.e. will show us what is inside data)
+            var formData = new FormData();
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            formData.append("file", this.file);
+
+            axios
+                .post("/upload", formData)
+                .then(function (response) {
+                    console.log("reponse from POST /upload", response);
+                })
+                .catch(function (err) {
+                    console.log("error in POST /upload", err);
+                });
+        },
+        handleChange: function (e) {
+            console.log("handleChangeRunning");
+            console.log("file: ", e.target.files(0)); // location of the selected file and  access to the file
+            this.file = e.target.files(0); // grab the selected file
+        },
     },
 });
 
