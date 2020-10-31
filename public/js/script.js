911c3cc00903e708eb5105348c11d6a00ea90004
Bledi Hasa <bledi.hasa@gmail.com>
2020-10-31 18:00:45 +0100

    styling changes

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,26 @@
 
 // IIFY
 (function () {
+    var inputs = document.querySelectorAll(".input-file");
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+
+        input.addEventListener("change", function (e) {
+            var fileName = "";
+            if (this.files && this.files.length > 1) {
+                fileName = (
+                    this.getAttribute("data-multiple-caption") || ""
+                ).replace("{count}", this.files.length);
+                fileName = e.target.value;
+            } else if (fileName) {
+                label.querySelector("span").innerHTML = fileName;
+            } else {
+                label.innerHTML = labelVal;
+            }
+        });
+    });
+
     Vue.component("my-component", {
         template: "#template",
         props: ["imageId"],
